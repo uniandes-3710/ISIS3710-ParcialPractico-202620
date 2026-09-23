818dbe05cd96cd1ae09fe02b601a6df670e54ead
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2026-09-23 16:53:56 -0500

    Update error files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AJiZFwMoHxQ1F146rlEh60OdCgv9RWMKo5OmGxvtwIw=\"\n}"
--- .next/dev/static/chunks/_00s_hiy._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__04kpziu._.css","static/chunks/src_components_UserMenu_tsx_0y5pop3._.js","static/chunks/node_modules_next_1bny_ld._.js","static/chunks/src_components_Header_tsx_1_ru7qq._.js","static/chunks/src_1t0sy2f._.js","static/chunks/src_1-gd38q._.js"],
-    source: "entry"
-});
